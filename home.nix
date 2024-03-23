b58be337fce70c5f05ceeb7fce2239b5600489ee
kallehed <kallehed@gmail.com>
2024-03-23 09:17:31 +0100

    add helix config

--- home.nix
+++ home.nix
@@ -63,6 +63,8 @@
   home.file.".config/waybar/config".source = ./waybar/config;
   home.file.".config/waybar/style.css".source = ./waybar/style.css;
 
+  home.file.".config/helix/config.toml".source = ./helix/config.toml;
+
   services.mako = {
     enable = true;
 #    backgroundColor = "#e0af68";
